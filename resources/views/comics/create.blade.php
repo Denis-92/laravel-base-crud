831629ca370d7d401509e6074e9088815d2d0532
Denis-92 <d.mozenic@gmail.com>
2022-11-09 00:52:48 +0100

    add title, description

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -12,6 +12,48 @@
 
     <form method="post">
         <div>
+            <label for="title">
+                Inserisci titolo...
+            </label>
+            <input type="text" name="title" id="title">
+        </div>
+
+        <div>
+            <textarea name="description" cols="25" rows="15">
+                Inserisci descrizione...
+            </textarea>
+            <input type="text" name="description" id="description">
+        </div>
+
+        <div>
+            <label for="">
+
+            </label>
+            <input type="text" name="" id="">
+        </div>
+
+        <div>
+            <label for="">
+
+            </label>
+            <input type="text" name="" id="">
+        </div>
+
+        <div>
+            <label for="">
+
+            </label>
+            <input type="text" name="" id="">
+        </div>
+
+        <div>
+            <label for="">
+
+            </label>
+            <input type="text" name="" id="">
+        </div>
+
+        <div>
             <label for="">
 
             </label>
