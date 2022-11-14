37e732b7e87ae4d81b4f0a8ca1098d2f72a0a8c1
Denis-92 <d.mozenic@gmail.com>
2022-11-14 15:07:26 +0100

    change parameters destroy function in controller

--- app/Http/Controllers/ComicsController.php
+++ app/Http/Controllers/ComicsController.php
@@ -100,7 +100,7 @@ class ComicsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Comic $comic)
     {
         //
     }
